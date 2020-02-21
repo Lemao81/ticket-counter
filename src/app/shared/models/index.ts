cd0cf820cada9ea3df39a2329300fd0bd81fdd61
Lemao81 <jr.berlin@web.de>
2020-02-21 22:14:57 +0100

    add enum + viewmodel

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -1,3 +1,4 @@
 export * from './requests/get-event-request';
 export * from './event';
 export * from './band';
+export * from './viewModels/event-details-view-model';
---
+++ src/app/shared/models/viewModels/event-details-view-model.ts
@@ -0,0 +1,8 @@
+export class EventDetailsViewModel {
+  date: Date;
+  bandName: string;
+  motto: string;
+  description: string;
+  vorbands: string[];
+  homepage: string;
+}
