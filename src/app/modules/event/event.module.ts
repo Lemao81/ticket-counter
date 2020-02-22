336f5aac6214295d04106e44bfe6370f2317d047
Lemao81 <jr.berlin@web.de>
2020-02-22 20:49:31 +0100

    add event page component

--- src/app/modules/event/event.module.ts
+++ src/app/modules/event/event.module.ts
@@ -4,9 +4,10 @@ import { SharedModule } from '../../shared/shared.module';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { EventListItemComponent } from './components/event-list-item/event-list-item.component';
 import { EventListComponent } from './components/event-list/event-list.component';
+import { EventPageComponent } from './components/event-page/event-page.component';
 
 @NgModule({
-  declarations: [EventListComponent, EventListItemComponent, EventDetailsComponent],
+  declarations: [EventListComponent, EventListItemComponent, EventDetailsComponent, EventPageComponent],
   imports: [SharedModule]
 })
 export class EventModule {}
---
+++ src/app/modules/event/event.routing.ts
@@ -0,0 +1,8 @@
+import { EventPageComponent } from './components/event-page/event-page.component';
+
+export const eventRoutes = [
+  {
+    path: '',
+    component: EventPageComponent
+  }
+];
