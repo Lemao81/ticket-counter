26a667ce672e68ed723153e9013e36ed4dabcf08
Lemao81 <jr.berlin@web.de>
2020-02-22 20:51:10 +0100

    adjust layout arrangement

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SideNavigationBarComponent } from './side-navigation-bar/side-navigation-bar.component';
-import { TopNavigationBarComponent } from './top-navigation-bar/top-navigation-bar.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { PageFrameComponent } from './page-frame/page-frame.component';
 import { RootComponent } from './root/root.component';
+import { SideNavigationBarComponent } from './side-navigation-bar/side-navigation-bar.component';
+import { TopNavigationBarComponent } from './top-navigation-bar/top-navigation-bar.component';
 
 @NgModule({
   declarations: [SideNavigationBarComponent, TopNavigationBarComponent, PageFrameComponent, RootComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, RouterModule]
 })
-export class LayoutModule { }
+export class LayoutModule {}
